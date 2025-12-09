7ea3b487d72a4fe5382c5326fcf3a84018ae8678
Ian Ding <17621762731@126.com>
2025-12-10 01:02:24 +0800

    P42 Particles Morphing Shader

--- src/shaders/particles/fragment.glsl
+++ src/shaders/particles/fragment.glsl
@@ -1,6 +1,11 @@
-void main()
-{
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    #include <tonemapping_fragment>
-    #include <colorspace_fragment>
+varying vec3 vColor;
+
+void main() {
+     vec2 uv = gl_PointCoord;
+     float distanceToCenter = length(uv - .5);
+     float alpha = .05 / distanceToCenter - .1;
+
+     gl_FragColor = vec4(vColor, alpha);
+     #include <tonemapping_fragment>
+     #include <colorspace_fragment>
 }
